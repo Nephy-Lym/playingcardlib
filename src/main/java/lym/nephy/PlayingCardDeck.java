70862e22efa06587b151ca90573e515cbbe4967a
Nephy-Lym <123238113+Nephy-Lym@users.noreply.github.com>
2023-02-13 18:14:29 -0800

    Create PlayingCardDeck Constructor

--- src/main/java/lym/nephy/PlayingCardDeck.java
+++ src/main/java/lym/nephy/PlayingCardDeck.java
@@ -1,7 +1,40 @@
 package lym.nephy;
 
+import java.util.Hashtable;
 import java.util.LinkedList;
 
-public class PlayingCardDeck extends LinkedList<PlayingCard> {
+public class PlayingCardDeck {
+    private LinkedList<PlayingCard> mDeck;
+
+    public PlayingCardDeck(Hashtable<Pip, Integer> config) {
+        mDeck = new LinkedList<>();
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 13; j++) {
+                Pip pip = Pip.values()[j];
+                mDeck.add(new PlayingCard(pip, config.get(pip), Suit.values()[i]));
+            }
+        }
+    }
+
+    public static void main(String[] pArgs) {
+        Hashtable<Pip, Integer> config = new Hashtable<>();
+        config.put(Pip.ACE, 1);
+        config.put(Pip.TWO, 2);
+        config.put(Pip.THREE, 3);
+        config.put(Pip.FOUR, 4);
+        config.put(Pip.FIVE, 5);
+        config.put(Pip.SIX, 6);
+        config.put(Pip.SEVEN, 7);
+        config.put(Pip.EIGHT, 8);
+        config.put(Pip.NINE, 9);
+        config.put(Pip.TEN, 10);
+        config.put(Pip.JACK, 10);
+        config.put(Pip.QUEEN, 10);
+        config.put(Pip.KING, 10);
+
+        PlayingCardDeck deck = new PlayingCardDeck(config);
+
+        System.out.print("debug");
+    }
 
 }
